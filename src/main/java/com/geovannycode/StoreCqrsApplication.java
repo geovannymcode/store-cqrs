58ecb32caa9bf436eb60f3bf67a2372ad1135d01
Geovanny Mendoza <me@geovannycode.com>
2025-08-22 12:00:00 -0500

    Commit retroactivo

--- src/main/java/com/geovannycode/StoreCqrsApplication.java
+++ src/main/java/com/geovannycode/StoreCqrsApplication.java
@@ -2,8 +2,10 @@ package com.geovannycode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.modulith.Modulithic;
 
 @SpringBootApplication
+@Modulithic
 public class StoreCqrsApplication {
 
 	public static void main(String[] args) {
