9fc109fa76ea75701b8b9eb1a5211d4b54acfdcd
Geovanny Mendoza <me@geovannycode.com>
2025-09-20 12:00:00 -0500

    Commit retroactivo

--- src/main/java/com/geovannycode/store/orders/domain/OrderStatus.java
+++ src/main/java/com/geovannycode/store/orders/domain/OrderStatus.java
@@ -1,4 +1,18 @@
 package com.geovannycode.store.orders.domain;
 
 public enum OrderStatus {
+    CREATED,
+    PAID,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED;
+
+    public boolean isActive() {
+        return this != DELIVERED && this != CANCELLED;
+    }
+
+    public boolean canBeCancelled() {
+        return this == CREATED || this == PAID || this == PROCESSING;
+    }
 }
